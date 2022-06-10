bb922f3e61557108c01b3ec2f7f58a369832aeee
NajwaEss <96242881+NajwaEss@users.noreply.github.com>
2022-06-10 15:07:01 +0100

    update name

--- src/Component/Profile/FullName.js
+++ src/Component/Profile/FullName.js
@@ -3,7 +3,7 @@
 function FullName() {
     return (
         <div >
-            <h3 style={{ marginLeft:"14%"}}>Hello My Name is Hajar</h3>
+            <h3 style={{ marginLeft:"14%"}}>Hello My Name is ESSAFI Najwa</h3>
         </div>
     );
 }
